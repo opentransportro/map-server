f1b7225b4d5d8c6a88b6fc64e534c03ae47c6d53
Vlad Vesa <vlad.vesa@outlook.com>
2021-11-10 20:31:53 +0200

    switched from vt-pbf to geobuf to fix serialisation of arrays
    https://github.com/mapbox/vt-pbf/blob/master/index.js#L111

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from "express";
-import * as vtpbf from "vt-pbf";
+import {encode} from "geobuf";
+import Pbf from "pbf";
 import cors from "cors";
 import schedule from "node-schedule";
 import * as zlib from "zlib";
@@ -118,7 +119,7 @@ const startService = async () => {
     if(req.params.layer =="romania-stop-map") {  // workarround that needs to be removed later
       geojsonData = { stops: tile };
     }
-    const data = Buffer.from(vtpbf.fromGeojsonVt(geojsonData));
+    const data = Buffer.from(encode(geojsonData, new Pbf()));
 
     res.setHeader("Content-Type", "application/x-protobuf");
     res.setHeader("Content-Encoding", "gzip");
